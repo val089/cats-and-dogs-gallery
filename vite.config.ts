bfac904d42668c9420bf888d41197d5be3e7a16c
Kamil Szerląg <kamilszerlag@gmail.com>
2023-04-22 12:31:03 +0200

    update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), pluginRewriteAll()],
   resolve: {
     alias: {
       '@app': path.resolve(__dirname, 'src'),
